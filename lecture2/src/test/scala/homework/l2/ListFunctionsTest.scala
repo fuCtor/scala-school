98418e4615d4383283fe3afd8c1f264779ce6576
Otts Aleksey Sergeevich <a.otts@tinkoff.ru>
2018-03-04 09:55:51 +0300

    Фикс теста

--- lecture2/src/test/scala/homework/l2/ListFunctionsTest.scala
+++ lecture2/src/test/scala/homework/l2/ListFunctionsTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class ListFunctionsTest extends FlatSpec with Matchers {
   "ListFunctions.fold" should "concatenate digits in List" in {
-    ListFunctions.fold("", List(1, 2, 3))((s, i) => s + " " + i) shouldBe "1 2 3"
+    ListFunctions.fold("", List(1, 2, 3))((s, i) => s + " " + i) shouldBe " 1 2 3"
   }
 
   "ListFunctions.fold" should "return start value if List is empty" in {
